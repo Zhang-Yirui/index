7f10de4914114d1eb89efb71aaf5bf6b5ea73a3a
Zhang-Yirui <Zhang-Yirui@users.noreply.github.com>
2024-08-13 15:15:12 +0000

    Deploying to main from @ Zhang-Yirui/index@3161c19e146d38232477df47c0a77611996f61e2 🚀

--- style/js/main.js
+++ style/js/main.js
@@ -1,4 +1,4 @@
-console.log('二〇二四年 七月(大) 初十 星期二 甲辰(龙)年 壬申月 己酉日 乙亥时 立秋 距离处暑还有9天');
+console.log('二〇二四年 七月(大) 初十 星期二 甲辰(龙)年 壬申月 己酉日 丙子时 立秋 距离处暑还有9天');
 window.addEventListener('load', () => {
   $('h1').addClass('ready');
   $('.bio').addClass('ready');
