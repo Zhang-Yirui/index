7cd8bf3872fc84638adfaf6d8c2c4692cd2b2217
Zhang-Yirui <Zhang-Yirui@users.noreply.github.com>
2026-01-01 15:20:41 +0000

    Deploying to main from @ Zhang-Yirui/index@780882ea76dbb9ac245c1aeaee82177151d4e9d7 🚀

--- style/js/main.js
+++ style/js/main.js
@@ -1,4 +1,4 @@
-window.onload = () => {console.log('二〇二五年 冬月(大) 十三 星期四 乙巳(蛇)年 戊子月 乙亥日 丁亥时 冬至 距离小寒还有4天');}
+window.onload = () => {console.log('二〇二五年 冬月(大) 十三 星期四 乙巳(蛇)年 戊子月 乙亥日 戊子时 冬至 距离小寒还有4天');}
 window.addEventListener('load', () => {
     $('h1').addClass('ready');
     $('.bio').addClass('ready');
