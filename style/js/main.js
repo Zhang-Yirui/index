a203b90714fe15ba5c7a4ec95c22b3b94cb718ac
Zhang-Yirui <Zhang-Yirui@users.noreply.github.com>
2026-04-01 16:54:27 +0000

    Deploying to main from @ Zhang-Yirui/index@08af255cd2682c101ae64eefa06caacdcef9a213 🚀

--- style/js/main.js
+++ style/js/main.js
@@ -1,4 +1,4 @@
-window.onload = () => {console.log('二〇二六年 二月(小) 十四 星期三 丙午(马)年 辛卯月 乙巳日 戊子时 春分 距离清明还有4天');}
+window.onload = () => {console.log('二〇二六年 二月(小) 十五 星期四 丙午(马)年 辛卯月 丙午日 戊子时 春分 距离清明还有3天');}
 window.addEventListener('load', () => {
     $('h1').addClass('ready');
     $('.bio').addClass('ready');
