268efa3203b7c5d76f466c15c2a6c4f8e071862b
Zhang-Yirui <Zhang-Yirui@users.noreply.github.com>
2026-07-23 08:27:43 +0000

    Deploying to main from @ Zhang-Yirui/index@192a2b2de498be433a15088d76d6246bfab26458 🚀

--- style/js/main.js
+++ style/js/main.js
@@ -1,4 +1,4 @@
-window.onload = () => {console.log('二〇二六年 六月(大) 初十 星期四 丙午(马)年 乙未月 戊戌日 己未时 大暑* 距离立秋还有-16天');}
+window.onload = () => {console.log('二〇二六年 六月(大) 初十 星期四 丙午(马)年 乙未月 戊戌日 庚申时 大暑* 距离立秋还有-16天');}
 window.addEventListener('load', () => {
     $('h1').addClass('ready');
     $('.bio').addClass('ready');
